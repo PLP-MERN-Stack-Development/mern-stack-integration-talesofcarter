6889623c26795e56247b45ce14e3e22ceb63d186
talesofcarter <kelvinjuma99@gmail.com>
2025-11-04 10:21:09 +0300

    Install and initialize Animate on Scroll

---
+++ client/src/components/Animate.jsx
@@ -0,0 +1,28 @@
+import { useEffect} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default function AOSInitializer() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+
+    AOS.refresh();
+
+    const handleRouteChange = () => {
+      AOS.refresh();
+    };
+
+    window.addEventListener("popstate", handleRouteChange);
+
+    return () => {
+      window.removeEventListener("popstate", handleRouteChange);
+    };
+  }, []);
+
+  return null;
+}
